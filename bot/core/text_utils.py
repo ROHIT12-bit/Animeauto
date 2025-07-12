9bf076e0085c54d484bdb2b9954ea4ef89505fcc
Lokesh <23ca1031@mvn.edu.in>
2025-07-12 18:15:54 +0530

    Update text_utils.py

--- bot/core/text_utils.py
+++ bot/core/text_utils.py
@@ -240,7 +240,7 @@ class TextEditor:
         return CAPTION_FORMAT.format(
             title=titles.get('english') or titles.get('romaji') or titles.get('native'),
             form=self.adata.get("format") or "N/A",
-            genres=", ".join(f"{x.replace(' ', ' ').replace('_', '-')}" for x in (self.adata.get('genres') or [])),
+            genres=", ".join(f"{GENRES_EMOJI[x]} #{x.replace(' ', '_').replace('-', '_')}" for x in (self.adata.get('genres') or [])),
             avg_score=f"{sc}%" if (sc := self.adata.get('averageScore')) else "N/A",
             status=self.adata.get("status") or "N/A",
             start_date=startdate or "N/A",
