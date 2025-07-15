6ff823d07717ab6fed1465cf01e95933108c3a24
Lokesh <23ca1031@mvn.edu.in>
2025-07-15 17:50:47 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -6,7 +6,7 @@ from aiofiles.os import remove as aioremove
 from traceback import format_exc
 from base64 import urlsafe_b64encode
 from time import time
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from bot import bot, bot_loop, Var, ani_cache, ffQueue, ffLock, ff_queued, ani_cache
